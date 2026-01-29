e751f3b7c7b59fb3b2dc69fe549520d4b38ca760
Insaniquarium <125063883+Insaniquarium@users.noreply.github.com>
2026-01-29 21:17:29 +0000

    frontend: display test contents for module pages
    Still a whole lot to do to make it make best use of screen space.
    Ideally, course pages shouldn't have a margin & padding on its contents
    like other pages do, but that's harder to fix with how that's done
    currently.

--- frontend/src/Module.jsx
+++ frontend/src/Module.jsx
@@ -1,3 +1,9 @@
-export default function Module() {
+import style from "./Module.module.scss";
 
+export default function Module() {
+	return (
+		<div className={style.Module}>
+			<iframe src="data:text/html,<h1>test</h1>" style={{background: "gray"}}></iframe>
+		</div>
+	);
 }
