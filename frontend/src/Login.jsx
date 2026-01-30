b3e2b01f85d593d17776924e9a39fd1873903cf4
Insaniquarium <125063883+Insaniquarium@users.noreply.github.com>
2026-01-30 01:41:08 +0000

    frontend: more on login page design

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,7 +1,11 @@
+import { useId } from "react";
 import Card from "./Card"
 import style from "./Login.module.scss";
 
 export default function Login() {
+	const emailId = useId();
+	const passwordId = useId();
+
 	function login(formData) {
 
 	}
@@ -10,11 +14,18 @@ export default function Login() {
 		<main className={style.Login}>
 			<Card>
 				<h1>Login</h1>
+				
 				<form action={login}>
-					<input type="email" name="email"/>
-					<input type="password" name="password"/>
+					<label htmlFor={emailId}>Email address:</label>
+					<input type="email" name="email" id={emailId}/>
+
+					<label htmlFor={passwordId}>Password:</label>
+					<input type="password" name="password" id={passwordId}/>
+
 					<input type="submit" value="Login"/>
 				</form>
+
+				<a href="#">Trouble logging in?</a>
 			</Card>
 		</main>
 	);
