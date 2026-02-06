4cc444d9f1e82cee79cb50145ef0b529aea474d6
Insaniquarium <125063883+Insaniquarium@users.noreply.github.com>
2026-02-06 21:52:58 +0000

    frontend: begin admin Users page

--- frontend/src/pages/admin/Users.jsx
+++ frontend/src/pages/admin/Users.jsx
@@ -1,6 +1,24 @@
 import { useTitle } from "#/hooks";
+import style from "./Users.module.scss";
 
 export function Users() {
 	useTitle(() => "Users");
-	return <></>;
+
+	return (
+		<div className={`${style.Users} page`}>
+			<h1>Users</h1>
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Joseph Lastname</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	);
 }
