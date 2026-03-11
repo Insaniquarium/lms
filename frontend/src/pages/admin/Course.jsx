060c21cd879a1dbd162754a9299b1a748d87fa17
Insaniquarium <125063883+Insaniquarium@users.noreply.github.com>
2026-03-11 04:48:56 +0000

    frontend: more work on admin Course page

--- frontend/src/pages/admin/Course.jsx
+++ frontend/src/pages/admin/Course.jsx
@@ -1,7 +1,9 @@
 import { useParams, Outlet } from "react-router";
 import { useTitle, useApi } from "#/hooks";
+import { formatDate } from "#/utils";
 import { NameBox } from "#/components/NameBox";
 import { TabBar, TabContent } from "#/components/Tabs";
+import { ModuleInfoRow } from "#/components/InfoRow";
 import style from "./Course.module.scss";
 
 export function Course() {
@@ -33,13 +35,79 @@ export function Course() {
 }
 
 Course.Info = function () {
-	return <p>info</p>;
+	const { courseID } = useParams();
+	const [course, loading] = useApi(api => api.getCourse(courseID));
+
+	if (loading)
+		return;
+
+	function modify(formData) {
+
+	}
+
+	return (
+		<div className={style.Course_Info}>
+			<p><b>Created</b>: {formatDate(course.created * 1000)}</p>
+
+			<form action={modify}>
+				<label>
+					Name:
+					<input type="text" name="name" placeholder="My Course" defaultValue={course.name} required/>
+				</label>
+
+				<label>
+					Description:
+					<textarea name="description" rows="10" placeholder="In this course, you will learn..." defaultValue={course.description} required></textarea>
+				</label>
+
+				{/* TODO: visibility, image, and fix layout */}
+			</form>
+		</div>
+	);
 }
 
 Course.Modules = function () {
-	return <p>modules</p>;
+	const { courseID } = useParams();
+	const [course, loading] = useApi(api => api.getCourse(courseID)); // TODO: getCourseModules
+
+	if (loading)
+		return;
+
+	return (
+		<div className={style.Course_Modules}>
+			{/* TODO: 'create' button */}
+			<ul>
+				{course.modules.map(module =>
+					<li key={module.id}>
+						<ModuleInfoRow module={module}/>
+					</li>
+				)}
+			</ul>
+		</div>
+	);
 }
 
 Course.Enrolments = function () {
-	return <p>enrolments</p>;
+	const { courseID } = useParams();
+	const [enrolments, loading] = useApi(api => api.getCourseEnrolments(courseID));
+
+	if (loading)
+		return;
+
+	return (
+		<div className={style.Course_Enrolments}>
+			<table>
+				<thead>
+					<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Enroled At</th>
+					</tr>
+				</thead>
+				<tbody>
+
+				</tbody>
+			</table>
+		</div>
+	);
 }
