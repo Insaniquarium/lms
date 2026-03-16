b2480afa125690cc87e1528bfe8e644bdf8ed24f
Insaniquarium <125063883+Insaniquarium@users.noreply.github.com>
2026-03-16 11:39:41 +0000

    frontend: get actual user name on admin home page

--- frontend/src/hooks.js
+++ frontend/src/hooks.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { API } from "./api";
 import { useAuth } from "./auth";
 
 export function useTitle(callback, dependencies = []) {
--- frontend/src/pages/admin/Home.jsx
+++ frontend/src/pages/admin/Home.jsx
@@ -1,11 +1,16 @@
-import { useTitle } from "#/hooks";
+import { useTitle, useApi } from "#/hooks";
 
 export function Home() {
+	const [user, loading] = useApi(api => api.getUser("me"));
+
 	useTitle(() => "Home");
+
+	if (loading)
+		return;
 
 	return (
 		<div className="page">
-			<h1>Welcome back, Joseph!</h1>
+			<h1>Welcome back, {user.first_name}!</h1>
 			{/* TODO: recent completion card */}
 			<p>Begin by choosing an option on the navigation bar.</p>
 		</div>
